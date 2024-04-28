dcfd40ee5e8cdc317bd95d7fcf6fcef04b20a012
drisaden <drisaden@gmail.com>
2024-04-28 01:17:40 +0100

    hero section about to be done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,18 @@ function toggleMenu() {
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    loop: true,
+    backDelay: 900,
+    backSpeed: 30,
+    smartBackspace: true,
+    typeSpeed: 40,
+    startDelay: 2000,
+  });
+
+const flipped = document.querySelector(".arrow-right");
+const matrix = new DOMMatrixReadOnly();
+const flippedMatrix = matrix.flipX();
+flipped.setAttribute("transform", flippedMatrix.toString());
